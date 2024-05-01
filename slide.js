67c0e00059e8f492f396f6d0d3dc41987ed3a282
rayssatessaro <135669844+rayssatessaro@users.noreply.github.com>
2024-05-01 20:48:07 -0300

    Add files via upload

---
+++ slide.js
@@ -0,0 +1,54 @@
+
+var radio = document.querySelector('.manual-btn')
+var cont = 1
+document.getElementById('radio1').checked = true
+setInterval(() => {
+    proximaImg()
+}, 5000)
+
+function proximaImg(){
+   cont ++ 
+   if(cont>4){
+     cont = 1
+   }
+   document.getElementById('radio'+ cont).checked = true
+}
+
+var header           = document.getElementById('header');
+    var navigationHeader = document.getElementById('navigation_header');
+    var content          = document.getElementById('content');
+    var showSidebar      = false;
+
+    function toggleSidebar()
+    {
+        showSidebar = !showSidebar;
+        if(showSidebar)
+        {
+            navigationHeader.style.marginLeft = '-10vw';
+            navigationHeader.style.animationName = 'showSidebar';
+            content.style.filter = 'blur(2px)';
+        }
+        else
+        {
+            navigationHeader.style.marginLeft = '-100vw';
+            navigationHeader.style.animationName = '';
+            content.style.filter = '';
+        }
+    }
+
+    function closeSidebar()
+    {
+        if(showSidebar)
+        {
+            showSidebar = true;
+            toggleSidebar();
+        }
+    }
+
+    window.addEventListener('resize', function(event) {
+        if(window.innerWidth > 768 && showSidebar) 
+        {  
+            showSidebar = true;
+            toggleSidebar();
+        }
+    });
